2b4dd47e65c599c82062d821689110e0fad970d0
pedrohenmonteiro <pedromont436@gmail.com>
2022-06-26 23:37:40 -0300

    Corrections

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -37,21 +37,21 @@ export function Subscribe() {
       <p className="mt-4 text-gray-200 leading-relaxed">Em only a week you will master in practice one of the most used technologies and in high demand to access the best opportunities in the job market</p>
       </div>
       
-      <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-        <strong className="text-2xl mb-6 block">Increva-se gratuitamente</strong>
+      <div className="p-8 bg-gray-700 border border-gray-500 rounded w-[390px]">
+        <strong className="text-2xl mb-6 block">Sign up for free </strong>
 
         <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
           <input
            className="bg-gray-900 rounded px-5 h-14"
            type="text"
-           placeholder="Seu nome completo"
+           placeholder="Your full name"
            value={name}
            onChange={e => setName(e.target.value)}
            />
           <input
            className="bg-gray-900 rounded px-5 h-14"
            type="text"
-           placeholder="Digite seu e-mail"
+           placeholder="Type your e-mail"
            value={email}
            onChange={e => setEmail(e.target.value)}
            />
@@ -61,7 +61,7 @@ export function Subscribe() {
             disabled={loading}
             className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
             >
-              Garantir minha vaga
+              Send my subscription
            </button>
         </form>
       </div>
